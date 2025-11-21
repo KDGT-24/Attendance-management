43b683abcd4c9a256f4dd0d78b448b1e3388ae0d
Ayumu Yamashita <a.yamashita0106@gmail.com>
2025-11-21 17:01:52 +0900

    First Commit

---
+++ src/main/java/com/example/Attendance_management_app/AttendanceManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Attendance_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Attendance_management_app/AttendanceManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Attendance_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
